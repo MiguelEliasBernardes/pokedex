e1db6c1e81139914e6cdfdb656636939082b7a95
Miguel Elias <miguelelias20050806@gmail.com>
2024-07-26 13:21:54 -0300

    att

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -6,32 +6,49 @@ function App() {
 
   const [pokemon,setPokemon] = useState({})
 
-  useEffect(() => {
-    async function loadPokemon(){
-      await api.get(`/1`)
-      .then((response) =>{
-        setPokemon(response.data)
-      })
-      .catch(() =>{
-        return "Erro"
-      })
-    }
+  async function loadPokemon(pokemon){
+    await api.get(`/${pokemon}`)
+    .then((response) =>{
+      setPokemon(response.data)
+    })
+    .catch(() =>{
+      return "Erro"
+    })
+  }
 
-    loadPokemon()
+  useEffect(() => {
+    loadPokemon('slowpoke')
   },[])
 
   console.log(pokemon)
   const sprite = pokemon.sprites
-
+  const types = pokemon.types || []
   return (
     <main>
+
+      <header>
+        <h1>Pokedéx</h1>
+      </header>
+
+
       <section>
       <div className="pokemon">
           {sprite && <img src={sprite.front_default}/>}
       </div>
 
       <div className="dados">
-          {pokemon.name}
+          <h2>Nome:{pokemon.name} - Número: {pokemon.id}</h2>
+          <ul>
+            {types.map((typeInfo, index) => (
+                <li key={index}>{typeInfo.type.name}</li>
+              ))}
+          </ul>
+          <div className="caracteristicas">
+            <p>Peso: {pokemon.weight / 10} Kg</p>
+            <p>Altura: {pokemon.height / 10} Metros</p>
+          </div>
+          
+
       </div>
       </section>
     </main>
