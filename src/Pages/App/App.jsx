c42f7a144c6e6b07ea28efc562d19b809bf91f10
Miguel Elias <miguelelias20050806@gmail.com>
2024-07-27 00:03:54 -0300

    requisições completas

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -5,6 +5,7 @@ import './app.css'
 function App() {
 
   const [pokemon,setPokemon] = useState({})
+  const [valor, setvalor] = useState("");
 
   async function loadPokemon(pokemon){
     await api.get(`/${pokemon}/`)
@@ -17,16 +18,22 @@ function App() {
   }
 
   useEffect(() => {
-    loadPokemon('slowpoke')
+    loadPokemon('1')
   },[])
 
-  console.log(pokemon)
+  const change = (event) => {
+    setvalor(event.target.value);
+  };
+
+  const click = () => {
+    loadPokemon(valor);
+  };
+
   const sprite = pokemon.sprites
   const types = pokemon.types || []
 
   return (
     <main>
-
       <header>
         <h1>Pokedéx</h1>
       </header>
@@ -39,22 +46,24 @@ function App() {
           </div>
 
           <div className="dados">
-              <h2>Nome:{pokemon.name} - Número: {pokemon.id}</h2>
+              <h2>{pokemon.id} - {pokemon.name}</h2>
               <ul>
                 {types.map((typeInfo, index) => (
-                    <li key={index}>{typeInfo.type.name}</li>
+                  <li key={index} className={typeInfo.type.name}>{typeInfo.type.name}</li>
+                    
                   ))}
               </ul>
               <div className="caracteristicas">
                 <p>Peso: {pokemon.weight / 10} Kg</p>
                 <p>Altura: {pokemon.height / 10} Metros</p>
+                
               </div>
-
+              <hr/>
               <div className="recolher">
                   <h3>Pesquise o pokemon(Digite o nome ou número)</h3>
                   <div className="recebe-dados">
-                    <input type="text" placeholder="pesquisar pokemon"/>
-                    <button type="button">Buscar</button>
+                    <input type="text" placeholder="pesquisar pokemon" value={valor} onChange={change}/>
+                    <button type="button" onClick={click}>Buscar</button>
                   </div>
               </div>
           </div>
